20ada701a1a46ad53e8a1760017c6daf4c2f348a
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2023-09-28 10:14:21 +0000

    [LeetCode Sync] Runtime - 82 ms (5.58%), Memory - 17 MB (100.00%)

---
+++ my-folder/0941-sort-array-by-parity/solution.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def sortArrayByParity(self, nums):
+        start = 0
+        end = len(nums) - 1
+
+        while start < end:
+            if nums[start] % 2 == 1 and nums[end] % 2 == 0:
+                nums[start], nums[end] = nums[end], nums[start]
+            if nums[start] % 2 == 0:
+                start += 1
+            if nums[end] % 2 == 1:
+                end -= 1
+
+        return nums
+
