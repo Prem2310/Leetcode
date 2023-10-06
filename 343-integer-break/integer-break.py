4c72c257c09e86a633a60f608978d4e357f20241
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2023-10-06 10:19:29 +0530

    Time: 40 ms (59.64%) | Memory: 16.3 MB (58.98%) - LeetSync

--- 343-integer-break/integer-break.py
+++ 343-integer-break/integer-break.py
@@ -1,11 +1,7 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        if n ==2:
-            res = 1
-            
-        elif n ==3:
-            res = 2
-
+        if n <=3:
+            res=n-1
         elif n%3 == 2:
             ans = n//3
             res = pow(3,ans) * 2
