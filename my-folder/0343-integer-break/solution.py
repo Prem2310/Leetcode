88a92d7c53a25b591adeec7a4507a38404289273
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2023-10-06 04:49:24 +0000

    [LeetCode Sync] Runtime - 40 ms (5.15%), Memory - 16.3 MB (100.00%)

---
+++ my-folder/0343-integer-break/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n <=3:
+            res=n-1
+        elif n%3 == 2:
+            ans = n//3
+            res = pow(3,ans) * 2
+        elif n%3 == 0:
+            ans = n//3
+            res = pow(3,ans)
+        else:
+            ans = n//3
+            res = pow(3,ans-1)*4
+        return res
+            
