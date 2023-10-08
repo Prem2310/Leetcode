8027e6a230b7a22d2dccf53c22236ffa702099df
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2023-10-08 22:26:06 +0530

    Time: 494 ms (30.26%) | Memory: 16.1 MB (79.33%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n):
+        def rec(n):
+            if n == 0:
+                return 0
+            if n == 1:
+                return 1
+            return rec(n-1) + rec(n-2)
+        return rec(n)
+    
