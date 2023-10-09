b3dc64daf7582d90c44b9209e0eea95c7f2a8721
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2023-10-09 10:20:43 +0530

    Time: 94 ms (6.42%) | Memory: 17.7 MB (50.97%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,54 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1,-1]
+        if len(nums)==1:
+            if nums[0]==target:
+                return [0,0]
+            else:
+                return [-1,-1]
+        def find(nums,target):
+            l=0
+            r=len(nums)-1
+            while l<=r:
+                mid=(l+r)//2
+                if nums[mid]==target:
+                    return mid
+                elif nums[mid]>target:
+                    r=mid-1
+                else:
+                    l=mid+1
+            return -1
+        def findleft(nums,target):
+            l=0
+            r=len(nums)-1
+            while l<=r:
+                mid=(l+r)//2
+                if nums[mid]==target:
+                    if mid==0 or nums[mid-1]!=target:
+                        return mid
+                    else:
+                        r=mid-1
+                elif nums[mid]>target:
+                    r=mid-1
+                else:
+                    l=mid+1
+            return -1
+        def findright(nums,target):
+            l=0
+            r=len(nums)-1
+            while l<=r:
+                mid=(l+r)//2
+                if nums[mid]==target:
+                    if mid==len(nums)-1 or nums[mid+1]!=target:
+                        return mid
+                    else:
+                        l=mid+1
+                elif nums[mid]>target:
+                    r=mid-1
+                else:
+                    l=mid+1
+            return -1
+        left=findleft(nums,target)
+        right=findright(nums,target)
+        return [left,right]
