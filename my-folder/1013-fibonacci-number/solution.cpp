1b6aa10fd821ea2b87b7cc575ba340250c801470
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-01-06 16:53:18 +0000

    [LeetCode Sync] Runtime - 13 ms (31.64%), Memory - 6.2 MB (100.00%)

--- my-folder/1013-fibonacci-number/solution.cpp
+++ my-folder/1013-fibonacci-number/solution.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-    
-   
-    // Base Condition.
-    if(n <= 1)
-    {
-        return n;
+        if(n<=1){
+            return n;
+        }
+        else{
+            return (fib(n-1)+fib(n-2));
+        }
+
     }
-
-    // Problem broken down into 2 functional calls
-    // and their results combined and returned.
-    int last = fib(n-1);
-    int slast = fib(n-2);
-    
-    return last + slast;
-
-    }   
 };
