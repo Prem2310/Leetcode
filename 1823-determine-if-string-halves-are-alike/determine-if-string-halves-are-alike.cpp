b7fc3eb62df28c0051903f5c1043b3a07918e40a
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-01-12 11:53:45 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (72.68%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    bool countvowels(char c){
-        unordered_set<char> vowels = {'A','E','I','O','U','a','e','i','o','u'};
-        return vowels.count(c);
-    }
     bool halvesAreAlike(string s) {
-        
-        int mid = s.length()/2;
-        int count1 =0;
-        int count2 =0;
-
-        for(int i =0;i<mid;i++){
-            if(countvowels(s[i])) count1++;
+        int n = s.size();
+        int c1=0,c2=0;
+        transform(s.begin(), s.end(), s.begin(), ::tolower); 
+        for(int  i = 0;i<n/2;i++)
+        {
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+                c1++;
         }
-        for(int i =mid;i<s.length();i++){
-            if(countvowels(s[i])) count2++;
-            
+        int c = 0;
+        for(int i = n/2;i<n;i++)
+        {
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+                c2++;
         }
-        return (count1 == count2);
-    }   
+        return (c1 == c2);
+    }
 };
