43f393346bb497346584cb7dc761985712a39dd1
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-01-17 23:06:59 +0530

    Time: 18 ms (58.04%) | Memory: 19.7 MB (19.47%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,10 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int j = 0;
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[j] = nums[i];
-                j++;
-            }
+            if (nums[i] != 0) nums[j++] = nums[i];
         }
         for (int i = j; i < nums.size(); i++) {
             nums[i] = 0;
