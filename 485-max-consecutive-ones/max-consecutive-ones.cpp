0a7260d6d081a889f3081cd374a9e85806251017
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-01-19 23:14:55 +0530

    Time: 32 ms (37.44%) | Memory: 38.6 MB (5.72%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -4,13 +4,13 @@ public:
         int maxi= 0,count = 0;
 
         for(int i=0;i<nums.size();i++){
-            if (nums[i]==1){
-                count+=1;
-            }
-            else{
+            if (nums[i]==0){
                 count = 0;
             }
-            maxi = max(maxi,count);
+            else{
+                count ++;
+                maxi = max(maxi,count);
+            }
         }
         return maxi;
     }
