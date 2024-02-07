8fda250260c0dc39f8a8ea28f58e40d9dc419460
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-02-07 11:40:55 +0530

    Time: 23 ms (12.64%) | Memory: 10.4 MB (32.43%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char, int> freq;
+        vector<int> freq(256, 0); // Considering all ASCII characters
+        
         for (char c : s) {
             freq[c]++;
         }
+
         sort(s.begin(), s.end(), [&](char a, char b) {
             return freq[a] > freq[b] || (freq[a] == freq[b] && a < b);
         });
         return s;
-
     }
 };
