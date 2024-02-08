5b9aace6b0e14585d9df8c4c5159c09767fc5269
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-02-08 18:30:01 +0530

    Time: 35 ms (93.57%) | Memory: 10.4 MB (38.33%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,23 +1,15 @@
 #include <vector>
 class Solution {
 public:
-    int arr[10001];
-    int helper(int n){
-        if (n==0){
-            return 0;
-        }
-        if (arr[n] != -1){
-            return arr[n];
-        }
-        int minCount =INT_MAX;
-        for(int i=1;i*i<=n;i++){
-            int res = 1 + helper(n-i*i);
-            minCount = min(minCount,res);
-        }
-        return arr[n] =  minCount;
-    }
     int numSquares(int n) {
-        memset(arr,-1,sizeof(arr));
-        return helper(n);   
+    vector<int> arr(n+1,INT_MAX);
+    arr[0] = 0;
+
+    for(int i=0;i<=n;i++){
+        for(int j = 1;j*j<=i;j++){
+            arr[i] = min(arr[i],1+arr[i-j*j]);
+        }
+    }    
+    return arr[n];
     }
 };
