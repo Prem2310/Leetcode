9a533d93fb6b2dec386ad86eb6b8df14be9c4c65
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-02-17 17:45:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (16.25%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        for(int i = 0; i <= high;){
+            if(nums[i] == 0){
+                swap(nums[i], nums[low]);
+                i++;
+                low++;
+            }
+            else if(nums[i] == 1){
+                i++;
+            }
+            else{
+                swap(nums[i], nums[high]);
+                high--;
+            }
+        }
         
     }
 };
