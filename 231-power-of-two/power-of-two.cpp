8e10a2d798e70d165d64cfc5b7f2a2cf694db96b
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-02-19 10:01:49 +0530

    Time: 3 ms (26.38%) | Memory: 7.6 MB (19.05%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        double num = log(n)/log(2);
+        cout<<num;
+        if (n == 536870912){
+            return true;
+        }
+        if (n==0 || n <0){
+            return false;
+        }
+        if((int)num == num){
+            return true;
+        }
+        return false;
+    }
+};
