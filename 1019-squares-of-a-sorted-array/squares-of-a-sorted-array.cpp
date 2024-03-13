772399ec8f7d442f5b3bc1359350d0ec9473d387
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-03-13 22:06:52 +0530

    Time: 16 ms (89.65%) | Memory: 28.2 MB (94.73%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            nums[i] = nums[i]*nums[i];
-        }
-        sort(nums.begin(),nums.end());
-        // int l = 0;
-        // int r = nums.size()-1;
-        // while(l<=r){
-        //     if(nums[l] > nums[r]){
-        //         int temp = nums[l];
-        //         nums[l] = nums[r];
-        //         nums[r] = temp;
-        //         l++;
-        //     }
-        //     else{
-        //         int temp = nums[l];
-        //         nums[l] = nums[r];
-        //         nums[r] = temp;
-        //         r--;
-        //     }
+        // for(int i=0;i<nums.size();i++){
+        //     nums[i] = nums[i]*nums[i];
         // }
-        return nums;
+        // sort(nums.begin(),nums.end());
+        int l = 0;
+        int r = nums.size()-1;
+        int idx = r;
+        vector<int> res(nums.size());
+        while(idx>=0){
+            if(abs(nums[l]) > abs(nums[r])){
+                res[idx] = nums[l] * nums[l];
+                l++;
+                idx--;
+            }
+            else{
+                res[idx] = nums[r]*nums[r];
+                r--;
+                idx--;
+            }
+        }
+        return res;
 
     }
 };
