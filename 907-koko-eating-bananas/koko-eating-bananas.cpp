6137faad71ee5f5fbd8e3be0766e6ccb2e6b676c
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-03-13 18:40:39 +0530

    Time: 33 ms (53.84%) | Memory: 21.4 MB (27.90%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -3,7 +3,12 @@ class Solution
 public:
     int minEatingSpeed(vector<int> &piles, int h)
     {
-        int l = 1, r = 1e9;
+        int l = 1;
+        int maxi = 0;
+        for(int i =0;i<piles.size();i++){
+            maxi = max(maxi,piles[i]);
+        }
+        int r = maxi;
         while (l < r)
         {
             int m = (l + r) / 2, t = 0;
