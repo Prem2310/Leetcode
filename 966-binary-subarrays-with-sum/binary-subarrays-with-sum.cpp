0c3689b77352ca568c57a263adc38f298ec63535
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-03-14 19:46:40 +0530

    Time: 78 ms (15.80%) | Memory: 40.4 MB (10.17%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int windowSize(vector<int>& nums, int goal){
-        if (goal < 0) return 0;
-        int res = 0;
-        int l = 0;
-        int sum = 0;
-        for(int i=0;i<nums.size();i++){
-            sum += nums[i];
-            while(sum > goal){
-                sum -= nums[l];
-                l++;
-            }
-            res += (i-l) +1;
-        }
-        return res;
-    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return windowSize(nums,goal) - windowSize(nums,goal-1);
-     }
+        int preSum = 0,count = 0;
+        map<int,int> mp;
+        mp[0] = 1;
+
+        for(int i=0;i<nums.size();i++){
+            preSum += nums[i];
+
+            int remove = preSum - goal;
+            count += mp[remove];
+            mp[preSum]++;
+        }
+        return count;     
+    }
 };
