2a89119e70c20cdcad1d07f65aed2080f609046f
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-03-15 20:39:35 +0530

    Time: 24 ms (63.94%) | Memory: 94.4 MB (70.05%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> A = nums1;
-        vector<int> B = nums2;
-
         int total = nums1.size() + nums2.size();
-        int half = (total + 1) / 2; 
+        int median = total / 2;
 
-        if (nums1.size() > nums2.size()) {
-            swap(A, B);
+        double currVal = 0;
+        int preVal = 0 ;
+
+        if (nums1.size()==0){
+            currVal = nums2[0];
         }
-
-        int l = 0, r = A.size();
-        while (l <= r) {
-            int i = (l + r) / 2;
-            int j = half - i;
-
-            int Aleft = (i > 0) ? A[i - 1] : INT_MIN;
-            int Aright = (i < A.size()) ? A[i] : INT_MAX;
-            int Bleft = (j > 0) ? B[j - 1] : INT_MIN;
-            int Bright = (j < B.size()) ? B[j] : INT_MAX;
-
-            if (Aleft <= Bright && Bleft <= Aright) {
-                if (total % 2) {
-                    return max(Aleft, Bleft);
-                } else {
-                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2.0;
+        else if(nums2.size()==0){
+            currVal = nums1[0];
+        }
+        else{
+            currVal = min(nums1[0] , nums2[0]);
+        }
+        int itrNum1 = 0, itrNum2 = 0;
+        for(int i = 0; i <= median ; i++ ){
+            preVal = currVal;
+            if (itrNum1 < nums1.size()  && itrNum2 < nums2.size()){
+                if(nums1[itrNum1] < nums2[itrNum2]){
+                    currVal = nums1[itrNum1];
+                    itrNum1 ++; 
                 }
-            } else if (Aleft > Bright) {
-                r = i - 1;
-            } else {
-                l = i + 1;
+                else {
+                    currVal = nums2[itrNum2];
+                    itrNum2 ++; 
+                }
+            }
+            else if(itrNum1 < nums1.size()){
+                currVal = nums1[itrNum1];
+                itrNum1 ++;
+            }
+            else{
+                currVal = nums2[itrNum2];
+                itrNum2 ++; 
             }
         }
 
-        return -1; 
+        if (total & 1){
+            return currVal;
+        }
+        return (currVal + preVal) / 2;
     }
 };
