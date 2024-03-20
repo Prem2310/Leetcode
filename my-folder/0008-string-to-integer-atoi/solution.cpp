3a57958d4c9f48e8a056b0368bc53e90abbd0d53
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-03-20 13:26:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (100.00%)

--- my-folder/0008-string-to-integer-atoi/solution.cpp
+++ my-folder/0008-string-to-integer-atoi/solution.cpp
@@ -8,13 +8,13 @@ public:
         while(i < s.length() && s[i] == ' ') i++;
         if(i < s.length() && (s[i] == '+' || s[i] == '-')){
             sign = (s[i++] == '-') ? -1 : 1;
-
         }
         while(i < s.length() && s[i] >= '0' && s[i] <= '9'){
             if(result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)){
                 return (sign == 1) ? INT_MAX : INT_MIN;
             }
-            result = result * 10 + (s[i++] - '0');
+            
+            result = result * 10 + (s[i++]-'0');
         }
         return result * sign;
     }
