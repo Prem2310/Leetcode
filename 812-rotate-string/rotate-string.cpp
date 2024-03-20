de62a735fd254b872f4ffae828085adad42e6049
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-03-20 17:55:24 +0530

    Time: 4 ms (10.22%) | Memory: 8.6 MB (12.57%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,8 +1,18 @@
-
-class Solution {
+class Solution
+{
 public:
-    bool rotateString(string A, string B) {
-        if (A.size() != B.size()) return false;
-        return (A + A).find(B) != string::npos;
+    bool rotateString(string A, string B)
+    {
+        if (A.size() != B.size())
+            return false;
+        if (A == B)
+            return true;
+        for (int i = 0; i < A.size(); i++)
+        {
+            if (A == B)
+                return true;
+            A = A.substr(1) + A[0];
+        }
+        return false;
     }
 };
