95510bf89e814abb7891de3fa392af18cb7475e6
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-03-20 14:28:42 +0530

    Time: 14 ms (7.24%) | Memory: 11.7 MB (5.06%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        string check = "abcdefghijklmnopqrstuvwxyz";
+        map<char,int>mp;
+        int p1 = 0;
+        int p2 = 0;
+        string s1 = "";
+        while(p2<s.length()){
+            if (mp.find(s[p2]) == mp.end()){
+                mp[s[p2]] = p1;
+                p1++;
+            }
+            s1 += to_string(mp[s[p2]]);
+            s1 += " ";
+            p2++;
+        }        
+        p1 = 0;
+        p2 = 0;
+        string t1 = "";
+        mp.clear();
+        while(p2<t.length()){
+            if (mp.find(t[p2]) == mp.end()){
+                mp[t[p2]] = p1;
+                p1++;
+            }
+            t1 += to_string(mp[t[p2]]);
+            t1 += " ";
+            p2++;
+        }      
+        cout << s1 << " s1" << endl;
+        cout << t1 << " t1 " << endl;
+        // string res = "";
+        // cout << to_string(1) <<" "<<endl; 
+        // res += to_string(1);
+        // cout << res << " res"; 
+        // return false;
+        return s1 == t1;
+    }
+};
