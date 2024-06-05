b6d08858e19992ae191c2fd9f7d872e45247cde0
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-06-05 15:28:36 +0530

    Time: 5 ms (76.27%) | Memory: 11.8 MB (41.98%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<string> ans;
+        vector<int> commonCount(26,INT_MAX);
+        for (const string s : words){
+            vector<int> count(26);
+            for(char c : s){
+                count[c-'a']++;
+            }
+            for(int i =0 ; i<26 ; i++){
+                commonCount[i] = min(commonCount[i],count[i]);
+            }
+        }
+
+        for(char c = 'a' ; c <= 'z'; c++){
+            for (int i = 0; i < commonCount[c-'a'] ; i++){
+                ans.push_back(string(1,c));
+            }
+        } 
+       return ans;
+    }
+};
