e1bb31b617bd4c42b45ee6797ffc14e5a33fdfd3
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-07-01 14:05:36 +0530

    Time: 3 ms (60.88%) | Memory: 10.6 MB (8.09%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count = 0;
+        for (int a : arr){
+            count = a % 2 == 1 ? count += 1:0;
+            if (count == 3) return true; 
+        }
+        return false;
+    }
+};
