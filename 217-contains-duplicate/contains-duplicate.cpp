16f1f5d3611e431d88fc5643b1e9806411e7e602
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-08-10 21:52:46 +0530

    Time: 96 ms (47.43%) | Memory: 73.4 MB (34.19%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for (int i =1;i<nums.size();i++){
-            if (nums[i]==nums[i-1]){
+        unordered_map<int,int> mp;
+        for(int num : nums){
+            if(mp.find(num)!=mp.end()){
                 return true;
             }
-        }
-        return false;
+            mp[num]++;
+        }             
+        return false;    
     }
 };
