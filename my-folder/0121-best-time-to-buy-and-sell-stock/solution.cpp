0558d8feeb859b81588f7c4399cb296d32094ea4
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-09-23 11:52:59 +0000

    [LeetCode Sync] Runtime - 74 ms (6.42%), Memory - 96.2 MB (99.97%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        if (n == 0) return 0;
-        
-        int dp[n][2];
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
-        
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
-            dp[i][1] = max(dp[i-1][1], -prices[i]);
-        }
-        
-        return dp[n-1][0];
+        int l = 0 , r = 1; 
+        int maxProfit = 0;
 
+        while(r < prices.size()){
+            if (prices[l] < prices[r]){
+                int profit = prices[r]-prices[l];
+                maxProfit = max(maxProfit, profit);
+            }
+            else{
+                l = r;
+            }
+            r++;
+        }
+
+        return maxProfit;
     }
 };
