c2b1ee9e40ef75033fdf6f5da0bbc7670c32bd14
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-09-26 14:17:18 +0000

    [LeetCode Sync] Runtime - 87 ms (14.68%), Memory - 73.4 MB (92.28%)

--- my-folder/0217-contains-duplicate/solution.cpp
+++ my-folder/0217-contains-duplicate/solution.cpp
@@ -3,11 +3,11 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> mp;
         for(int num : nums){
-            if(mp.find(num)!=mp.end()){
+            if(mp.find(num) != mp.end()){
                 return true;
             }
             mp[num]++;
-        }             
-        return false;    
+        }  
+        return false;
     }
 };
