4ba88eda023be7ff5ca935fd142ca9c11cc3fe63
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-09-28 12:26:58 +0530

    Time: 17 ms (15.03%) | Memory: 18.6 MB (13.90%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        
-        // Count the frequency of each number
-        for (auto i : nums) {
-            mp[i]++;
-        }
-        
-        // Create a vector of pairs to store frequency and number
-        vector<pair<int, int>> freqNumPairs;
-        for (auto entry : mp) {
-            freqNumPairs.push_back({entry.second, entry.first});
-        }
-        
-        // Sort the vector based on frequency in descending order
-        sort(freqNumPairs.rbegin(), freqNumPairs.rend());
-        
-        // Extract the top k frequent numbers
+        unordered_map<int,int> mp;
         vector<int> ans;
-        for (int i = 0; i < k; i++) {
-            ans.push_back(freqNumPairs[i].second);
+        vector<pair<int,int>> freqPair;
+
+        for(int num : nums){
+            mp[num]++;
         }
-        
+ 
+        for(auto x:mp){
+            freqPair.push_back({x.second , x.first});
+        }
+        sort(freqPair.rbegin(),freqPair.rend());
+
+        for (int i = 0 ; i < k ; i++){
+            ans.push_back(freqPair[i].second);
+        }       
         return ans;
     }
+
 };
