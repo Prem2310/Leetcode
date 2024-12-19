3f01174eb366e1a3691c01070b8b3719ced99729
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-12-19 18:39:48 +0000

    [LeetCode Sync] Runtime - 11 ms (77.18%), Memory - 17.8 MB (100.00%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,11 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = list(s)
-        t = list(t)
-        s.sort()
-        t.sort()
-        if s == t:
-            return True
-        else:
+        if len(s) != len(t):    
             return False
-        
+        arr = [0]*26
+        for c in s:
+            arr[ord(c) - ord('a')]+=1
+            
+        for c in t:
+            if arr[ord(c)-ord('a')] == 0: 
+                return False
+            arr[ord(c)-ord('a')]-=1
+            
+        return True
