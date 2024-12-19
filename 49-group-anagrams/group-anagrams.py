8ca0a3213b64c5739917447b2eb9873fd654c6d3
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-12-20 00:26:35 +0530

    Time: 11 ms (80.74%) | Memory: 20.5 MB (41.09%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+  def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagrams = defaultdict(list)
+        
+        for s in strs:
+            key = ''.join(sorted(s))
+            anagrams[key].append(s)
+
+        return list(anagrams.values())
