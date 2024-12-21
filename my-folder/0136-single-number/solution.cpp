09f2982b16e3162a77561021cb76c76fb4cadead
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-12-21 09:05:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.7 MB (59.31%)

--- my-folder/0136-single-number/solution.cpp
+++ my-folder/0136-single-number/solution.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    int singleNumber(std::vector<int>& nums) {
-        int result = 0;
-        for(int i=0;i<nums.size();i++){
-            result = result ^ nums[i];
-        }
-        return result;
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(int num : nums){
+            res ^= num;
+        }   
+        return res;
     }
 };
-
