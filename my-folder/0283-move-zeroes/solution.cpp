848435b0826acd630af8eb3dda9582647bf7aa94
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-12-21 08:44:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.8 MB (83.33%)

--- my-folder/0283-move-zeroes/solution.cpp
+++ my-folder/0283-move-zeroes/solution.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int j = 0;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] != 0 ){
-                nums[j++] = nums[i];
+        for (int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] != 0){
+                nums[j++] = nums[i]; 
             }
         }
-
-        for(int i = j; i< nums.size();i++){
+        for(int i = j ; i < nums.size() ; i++){
             nums[i] = 0;
         }
+        
     }
 };
