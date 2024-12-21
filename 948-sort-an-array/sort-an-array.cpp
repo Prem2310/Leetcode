66d4f8fc6230c6e1ee4a0d3c3c8bbc2266aaa841
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-12-21 13:29:39 +0530

    Time: 226 ms (51.16%) | Memory: 146.9 MB (36.06%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,49 +1,38 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> sortArray(vector<int> &nums)
-    {
-  
-        mergeSort(nums, 0, nums.size() - 1);
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums,0,nums.size()-1);
         return nums;
     }
-
-    void mergeSort(vector<int> &nums, int l, int r)
-    {
-        if (l < r)
-        {
-            int m = l + (r - l) / 2;
-            mergeSort(nums, l, m);
-            mergeSort(nums, m + 1, r);
-            merge(nums, l, m, r);
+    void mergeSort(vector <int>& A,int l , int r){
+        if(l >= r){
+            return ;
         }
+        const int m = ( l + r) / 2;
+        mergeSort(A , l , m);
+        mergeSort(A , m + 1 , r);
+        merge(A , l , m , r);
     }
+    void merge(vector <int>& A,int l ,int mid, int r){
+        vector<int> sorted(r-l+1);
+        int k = 0;
+        int i = l;
+        int j = mid + 1;
 
-    void merge(vector<int> &nums, int l, int m, int r)
-    {
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        vector<int> L(n1), R(n2);
-
-        for (int i = 0; i < n1; i++)
-            L[i] = nums[l + i];
-        for (int i = 0; i < n2; i++)
-            R[i] = nums[m + 1 + i];
-
-        int i = 0, j = 0, k = l;
-
-        while (i < n1 && j < n2)
-        {
-            if (L[i] <= R[j])
-                nums[k++] = L[i++];
-            else
-                nums[k++] = R[j++];
+        while (i <= mid && j <= r){
+            if(A[i] < A[j]){
+                sorted[k++] = A[i++];
+            }
+            else{
+                sorted[k++] = A[j++];
+            }
         }
-
-        while (i < n1)
-            nums[k++] = L[i++];
-        while (j < n2)
-            nums[k++] = R[j++];
+        while (i<=mid){
+            sorted[k++]  = A[i++];
+        }
+        while (j <= r){
+            sorted[k++]  = A[j++];
+        }
+        copy(sorted.begin(), sorted.end(), A.begin() + l);
     }
 };
