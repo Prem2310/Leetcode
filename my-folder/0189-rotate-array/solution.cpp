9d0119dc5feca495321d450f1ff8a24ca5e10c9f
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-12-21 08:40:53 +0000

    [LeetCode Sync] Runtime - 3 ms (7.91%), Memory - 29.6 MB (70.73%)

--- my-folder/0189-rotate-array/solution.cpp
+++ my-folder/0189-rotate-array/solution.cpp
@@ -3,9 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         k = k % nums.size();
 
-        reverse(nums.begin(),nums.begin() + (nums.size()-k));
-        reverse(nums.begin() + (nums.size()-k) , nums.end());
+        reverse(nums.begin() , nums.begin() + (nums.size() - k));
+        reverse( nums.begin() + (nums.size() - k), nums.end());
         reverse(nums.begin() , nums.end());
-
     }
 };
