deaedf27a379b8ee7b61761beb06881a2b2d7cc6
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-12-21 15:01:28 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (14.51%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int n = (int)nums.size() / 2; 
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-            if (mp[nums[i]] > n) { 
-                return nums[i]; 
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        for (int num : nums) {  // Use 'num' instead of 'n' to avoid confusion
+            mp[num] += 1;
+            if (mp[num] > n / 2) {  // Compare with 'n / 2' instead of 'num / 2'
+                return num;
             }
         }
-        return -1; 
-        }
+        return -1;  // This line is just for safety; by problem constraints, a majority element always exists.
+    }
 };
