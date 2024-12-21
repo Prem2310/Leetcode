d00b4dbbaed9cadd573a53df4bb2fa88c990b585
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-12-21 08:20:31 +0000

    [LeetCode Sync] Runtime - 596 ms (38.15%), Memory - 295.8 MB (9.02%)

--- my-folder/0948-sort-an-array/solution.cpp
+++ my-folder/0948-sort-an-array/solution.cpp
@@ -1,49 +1,37 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> sortArray(vector<int> &nums)
-    {
-  
-        mergeSort(nums, 0, nums.size() - 1);
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums , 0 , nums.size()-1);
         return nums;
     }
-
-    void mergeSort(vector<int> &nums, int l, int r)
-    {
-        if (l < r)
-        {
-            int m = l + (r - l) / 2;
-            mergeSort(nums, l, m);
-            mergeSort(nums, m + 1, r);
-            merge(nums, l, m, r);
-        }
+    void mergeSort(vector<int> &nums ,int l , int r){
+        if (l >= r) return;
+        int mid = (l + r) / 2;
+        mergeSort(nums , l , mid);
+        mergeSort(nums , mid + 1 , r);
+        merge(nums , l , mid , r);
     }
+    void merge(vector<int> &nums , int l, int mid , int r){
+        vector<int> temp;
+        int left = l ;
+        int right = mid + 1;
 
-    void merge(vector<int> &nums, int l, int m, int r)
-    {
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        vector<int> L(n1), R(n2);
-
-        for (int i = 0; i < n1; i++)
-            L[i] = nums[l + i];
-        for (int i = 0; i < n2; i++)
-            R[i] = nums[m + 1 + i];
-
-        int i = 0, j = 0, k = l;
-
-        while (i < n1 && j < n2)
-        {
-            if (L[i] <= R[j])
-                nums[k++] = L[i++];
-            else
-                nums[k++] = R[j++];
+        while(left <=mid && right <= r){
+            if(nums[left] <=nums[right]){
+                temp.push_back(nums[left++]);
+            }
+            else{
+                temp.push_back(nums[right++]);
+            }
         }
 
-        while (i < n1)
-            nums[k++] = L[i++];
-        while (j < n2)
-            nums[k++] = R[j++];
+        while(left <= mid){
+            temp.push_back(nums[left++]);
+        }
+        while(right <= r){
+            temp.push_back(nums[right++]);
+        }
+
+        copy(temp.begin() ,temp.end() , nums.begin() + l);
     }
 };
