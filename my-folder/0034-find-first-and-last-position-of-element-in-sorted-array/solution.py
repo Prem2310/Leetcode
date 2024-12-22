f84d4f9ed01c669d626ecd8e141c5ee01a63edf6
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-12-22 17:13:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (100.00%)

--- my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
+++ my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -1,43 +1,28 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return [-1,-1]
-        if len(nums)==1:
-            if nums[0]==target:
-                return [0,0]
+        left , right = 0 , len(nums) - 1
+        leftIdx , rightIdx = -1 , -1
+        
+        while(left <= right):
+            mid = (left + right )//2
+            if(nums[mid] == target):
+                leftIdx = mid
+                right = mid -1
+            elif(nums[mid] > target):
+                right = mid -1
             else:
-                return [-1,-1]
- 
-        def findleft(nums,target):
-            l=0
-            r=len(nums)-1
-            while l<=r:
-                mid=(l+r)//2
-                if nums[mid]==target:
-                    if mid==0 or nums[mid-1]!=target:
-                        return mid
-                    else:
-                        r=mid-1
-                elif nums[mid]>target:
-                    r=mid-1
-                else:
-                    l=mid+1
-            return -1
-        def findright(nums,target):
-            l=0
-            r=len(nums)-1
-            while l<=r:
-                mid=(l+r)//2
-                if nums[mid]==target:
-                    if mid==len(nums)-1 or nums[mid+1]!=target:
-                        return mid
-                    else:
-                        l=mid+1
-                elif nums[mid]>target:
-                    r=mid-1
-                else:
-                    l=mid+1
-            return -1
-        left=findleft(nums,target)
-        right=findright(nums,target)
-        return [left,right]
+                left = mid + 1
+        
+        
+        left , right = 0 , len(nums) - 1
+        while(left <= right):
+            mid = (left + right )//2
+            if(nums[mid] == target):
+                rightIdx = mid
+                left = mid + 1
+            elif(nums[mid] > target):
+                right = mid -1
+            else:
+                left = mid + 1
+        
+        return [leftIdx , rightIdx]
