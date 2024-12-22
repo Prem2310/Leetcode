d885d0475b149c3be40d7f6909ad510e124e8a0c
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2024-12-22 13:30:09 +0000

    [LeetCode Sync] Runtime - 479 ms (92.50%), Memory - 20.9 MB (99.96%)

--- my-folder/0015-3sum/solution.py
+++ my-folder/0015-3sum/solution.py
@@ -1,21 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
         nums.sort()
-        res = []
-        n = len(nums)
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            l = i+1
-            r = n-1
-            while l < r:
-                if nums[i] + nums[l] + nums[r] == 0:
-                    res.append([nums[i],nums[l],nums[r]])
-                    l += 1
-                    while l < r and nums[l] == nums[l-1]:
+
+        for i in range(len(nums)-2):
+            if (i==0 or (i>0 and nums[i] != nums[i-1])):
+                target = -nums[i]
+                l = i+1
+                r = len(nums) - 1
+
+                while(l < r ):
+                    curr = nums[l] + nums[r]
+                    if(curr == target):
+                        ans.append([nums[i],nums[l],nums[r]])
+                        while(l<r and nums[l]==nums[l+1]): l += 1
+                        while(l<r and nums[r]==nums[r-1]): r -= 1
                         l += 1
-                elif nums[i] + nums[l] + nums[r] < 0:
-                    l += 1
-                else:
-                    r -= 1
-        return res
+                        r -= 1
+                    elif (curr > target):
+                        r -= 1
+                    else:
+                        l += 1
+
+        return ans
