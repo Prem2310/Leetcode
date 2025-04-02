9e0f01783eb289be5eebd19bab2dc7cddb7ea6a7
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2025-04-02 06:32:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.8 MB (37.19%)

---
+++ my-folder/3154-maximum-value-of-an-ordered-triplet-i/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  long long maximumTripletValue(vector<int>& nums) {
+    long res = 0;
+    int maxDiff = 0;
+    int maxNum = 0;
+
+    for (const int num : nums) {
+      res = max(res, static_cast<long>(maxDiff) * num);
+      maxDiff = max(maxDiff, maxNum - num);
+      maxNum = max(maxNum, num);
+    }
+
+    return res;
+  }
+};
