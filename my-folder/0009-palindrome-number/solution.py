5c32c5a33e7ef3c6cca59cdad1f272fc1baee500
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2026-08-04 20:46:15 +0000

    [LeetCode Sync] Runtime - 6 ms (67.75%), Memory - 19.4 MB (18.67%)

--- my-folder/0009-palindrome-number/solution.py
+++ my-folder/0009-palindrome-number/solution.py
@@ -1,15 +1,13 @@
 class Solution:
-   def isPalindrome(self, x: int) -> bool:
-        if x >= 2**31-1 or x <= -2**31:
-            return 0
-        else:
-            strg = str(x)
-            if strg[0] == '-':
-                return False
-            y = strg[::-1]
-            z = int(y)
-            if z == x:
-                return True
-            else:
-                return False
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
 
+        reverse = 0
+        xcopy = x
+
+        while x > 0:
+            reverse = (reverse * 10) + (x % 10)
+            x //= 10
+        
+        return reverse == xcopy
